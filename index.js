2a148d5c703e302e49a9bdecbc42450f9c7a9350
Rory Claasen <rory@claasen.biz>
2017-02-03 13:12:40 +0000

    updated code, changed file names, added dependencies

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var pkg = require(path.join(__dirname, 'package.json'));
 var chalk = require('chalk');
+var parser = require('gitignore-parser');
 
 var program = require('commander');
 program.version(pkg.version)
@@ -18,9 +19,38 @@ if (fs.existsSync(program.directory)) {
     program.help();
     process.exit(0);
 }
+var gitignore = undefined;
+if (fs.existsSync('.gitignore')) {
+    gitignore = parser.compile(fs.readFileSync('.gitignore', 'utf8'));
+    console.log('Filtered using .gitignore');
+}
 
-fs.readdir(start, (err, files) => {
+console.log('Scanning %s', start);
+var files = 0, folders = 1, lines = 0;
+function scan(current) {
+    var files = fs.readdirSync(current);
+    if (gitignore != undefined) files = files.filter(gitignore.accepts);
     files.forEach(file => {
-        console.log(file);
+        if (fs.existsSync(file)) {
+            var stat = fs.lstatSync(file);
+            if (stat.isDirectory()) {
+                folders++;
+                scan(file);
+            } else if (stat.isFile()) {
+                files++;
+                read(file);
+            }
+            if (program.output) console.log(chalk.gray(file));
+        }
     });
-})
+}
+
+function read(file) {
+    var contents = fs.readFileSync(file, 'utf8');
+    //for ()
+}
+
+scan(start);
+console.log("Files: %s", files);
+console.log("Folders: %s", folders);
+console.log("Lines: %s", lines);
