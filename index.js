cf75c20f42c408d07204f051dffcd96d252e2041
Rory Claasen <rory@claasen.biz>
2017-02-06 20:28:50 +0000

    updated language oject, extention list to object, added whitespace counter (basic), bump version

--- index.js
+++ index.js
@@ -31,92 +31,98 @@ if (fs.existsSync(gitignorePath) && !program.nogitignore) {
     console.log('Filtering using .gitignore');
 }
 
-var extensions = [
-    "asm",
-    "brs",
-    "bat", "cmd",
-    "c",
-    "cc",
-    "clj",
-    "cljs",
-    "coffee",
-    "cpp",
-    "cr",
-    "cs",
-    "css",
-    "cxx",
-    "dart",
-    "erl",
-    "go",
-    "groovy",
-    "gs",
-    "h",
-    "handlebars", "hbs",
-    "hpp",
-    "hr",
-    "hs",
-    "html", "htm",
-    "hx",
-    "hxx",
-    "hy",
-    "iced",
-    "ino",
-    "jade",
-    "java",
-    "jl",
-    "js",
-    "jsx",
-    "kt",
-    "kts",
-    "less",
-    "lua",
-    "ls",
-    "ml",
-    "mli",
-    "mochi",
-    "monkey",
-    "mustache",
-    "nix",
-    "nim",
-    "nut",
-    "php", "php5",
-    "pl",
-    "py",
-    "r",
-    "rb",
-    "rkt",
-    "rs",
-    "sass",
-    "scala",
-    "scss",
-    "sh",
-    "styl",
-    "svg",
-    "swift",
-    "ts",
-    "tsx",
-    "vb",
-    "xml",
-    "yaml",
-    "m",
-    "mm"
-]
+var ignoreDir = ['node_modules', '.git'];
 
-function extensionsContains(extention) {
-    var i = extensions.length;
+function ignoreContains(folder) {
+    var i = ignoreDir.length;
     while (i--) {
-        if (extensions[i] === extention) return true;
+        if (ignoreDir[i] === folder) return true;
     }
     return false;
+}
+
+var extensions = {
+    'asm': 'Assembly',
+    'brs': 'Brightscript',
+    'bat': 'Batch', 'cmd': 'Batch',
+    'c': 'C / C++',
+    'cc': 'C / C++',
+    'clj': 'Clojure',
+    'cljs': 'Clojure',
+    'coffee': 'CoffeeScript',
+    'cpp': 'C / C++',
+    'cr': 'Crystal',
+    'cs': 'C#',
+    'css': 'CSS',
+    'cxx': 'C / C++',
+    'dart': 'Dart',
+    'erl': 'Erlang',
+    'go': 'Go',
+    'groovy': 'Groovy',
+    'gs': 'JavaScript',
+    'h': 'C / C++',
+    'handlebars': 'Handlebars', 'hbs': 'Handlebars',
+    'hpp': 'C / C++',
+    'hr': undefined,
+    'hs': 'Haskell',
+    'html':'HTML', 'htm': 'HTML',
+    'hx': 'Haxe',
+    'hxx': 'C / C++',
+    'hy': 'Hy',
+    'iced': 'CoffeeScript',
+    'ino': undefined,
+    'jade': 'Jade',
+    'java': 'Java',
+    'jl': 'Julia',
+    'js': 'Java Script',
+    'jsx': 'JSX',
+    'kt': 'Kotlin',
+    'kts': 'Kotlin',
+    'less': 'CSS',
+    'lua': 'Lua',
+    'ls': 'LiveScript',
+    'ml': 'OCaml',
+    'mli': 'OCaml',
+    'mochi': 'Mochi',
+    'monkey': 'Monkey',
+    'mustache': 'HTML',
+    'nix': 'Nix',
+    'nim': 'Nim',
+    'nut': 'Squirrel',
+    'php': 'PHP', 'php5': 'PHP',
+    'pl': 'Perl 5',
+    'py': 'Python',
+    'r': 'R',
+    'rb': 'Ruby',
+    'rkt': 'Racket',
+    'rs': 'Rust',
+    'sass': 'CSS',
+    'scala': 'Scala',
+    'scss': 'CSS',
+    'sh': 'Shell',
+    'styl': 'CSS',
+    'svg': 'SVG',
+    'swift': 'Swift',
+    'ts': 'Typescript',
+    'tsx': 'Typescript',
+    'vb': 'Visual Basic',
+    'xml': 'XML',
+    'yaml': 'Yaml',
+    'm': 'Objective-C / Objective-C++',
+    'mm': 'Objective-C / Objective-C++'
+}
+
+function extensionsContains(extension) {
+    return extension in extensions;
 }
 
 function combine(data1, data2) {
     var result = {};
     for (var key in data1) result[key] = data1[key];
     for (var key in data2) {
-        if (key in result) result[key] += data2[key];
-        else result[key] = data2[key];
-
+        if (key in result) {
+            if (typeof result[key] == 'number') result[key] += data2[key];
+        } else result[key] = data2[key];
     }
     return result;
 }
@@ -130,7 +136,8 @@ function scan(current, callback) {
     if (gitignore != undefined) files = files.filter(gitignore.accepts);
 
     files.forEach(next => {
-        if (next === '.git') return;
+        if (ignoreContains(next)) return;
+        // if (next === '.git') return;
         var next = path.join(current, next);
         if (program.args != undefined) if (program.args.length > 0) {
             var match = false;
@@ -151,7 +158,13 @@ function scan(current, callback) {
                     cFiles += data.files;
                     cFolders += data.folders;
                     cLines += data.lines;
-                    languages = combine(languages, data.languages);
+                    Object.keys(data.languages).forEach(function(key) {
+                        var val = data.languages[key];
+                        if (!(key in languages)) languages[key] = { 'lines': 0, 'whitespace': 0 };
+
+                        var combined = combine(languages[key], val);
+                        languages[key] = combined;
+                    });
                 });
             } else if (stat.isFile()) {
                 var file = next.split('/').pop();
@@ -160,8 +173,11 @@ function scan(current, callback) {
                     cFiles++;
                     read(next, function(data) {
                         cLines += data.lines;
-                        if (ext in languages) languages[ext] += data.lines;
-                        else languages[ext] = data.lines;
+                        if (!(ext in languages)) languages[ext] = { 'lines': 0, 'whitespace': 0 };
+                        languages[ext] = combine(languages[ext], {
+                            'lines': data.lines,
+                            'whitespace': data.whitespace
+                        });
                     });
                 }
             }
@@ -176,15 +192,16 @@ function scan(current, callback) {
 }
 
 function read(file, callback) {
-    var lines = 0;
+    var lines = 0, white = 0;
     try {
         var contents = fs.readFileSync(file, 'utf8');
 
-        lines = contents.split(endOfLine).length;
+        // lines = contents.split(endOfLine).length;
 
-        // for (String line: contents.split(endOfLine)) {
-        //     lines ++;
-        // }
+        contents.split(endOfLine).forEach((line) => {
+            lines ++;
+            if (line.length === 0) white ++;
+        });
 
         if (program.output) console.log('file > %s %s line(s)', chalk.cyan(file), lines);
     } catch (e) {
@@ -193,33 +210,54 @@ function read(file, callback) {
         if (program.output) console.error(chalk.red(e));
     }
     callback({
-        'lines': lines
+        'lines': lines,
+        'whitespace': white
     });
 }
 
 console.log('Scanning \'%s\'', start);
 scan(start, function(data) {
-    console.log();
-    console.log('Folders Scanned: %s', data.folders);
-    console.log('     Files Read: %s', data.files);
-    console.log('    Total Lines: %s', data.lines);
-    var percent = 0;
+    try {
+        console.log();
+        console.log('Folders Scanned: %s', data.folders);
+        console.log('     Files Read: %s', data.files);
+        console.log('    Total Lines: %s', data.lines);
+        var percent = 0;
 
-    var langData = [];
-    for (var lang in data.languages) {
-        var outData = {};
-        outData.extention = lang;
-        outData.lines = data.languages[lang];
-        outData.percent = ((data.languages[lang] / data.lines) * 100).toFixed(3) + '%';
-        langData.push(outData);
-    }
-
-    var columns = columnify(langData, {
-        minWidth: 20,
-        config: {
-            description: {maxWidth: 30}
+        var langData = [];
+        for (var lang in data.languages) {
+            var outData = {};
+            outData.language = extensions[lang];
+            if (outData.language == undefined) outData.extention = lang;
+            outData.lines = data.languages[lang].lines;
+            outData.whitesapce = chalk.gray(data.languages[lang].whitespace);
+            //outData.percent = ((data.languages[lang].lines / data.lines) * 100).toFixed(3) + '%';
+            var index = -1;
+            langData.forEach(function (item) {
+                if (item.language == outData.language) {
+                    index = langData.indexOf(item);
+                    outData = combine(item, outData);
+                    return;
+                }
+            });
+            if (index >= 0) {
+                langData[index] = outData;
+            } else langData.push(outData);
         }
-    });
-    console.log();
-    console.log(columns)
+        langData.forEach(function (item) {
+            item.percent = ((item.lines / data.lines) * 100).toFixed(3) + '%';
+        });
+
+        var columns = columnify(langData, {
+            minWidth: 20,
+            config: {
+                description: {maxWidth: 30}
+            }
+        });
+        console.log();
+        console.log(columns)
+
+    } catch(e) {
+        console.error(e);
+    }
 });
